c9a9135eed5172be4fc341ea88e88ad89abcc0b1
Shivang Singh <shivangsingh@gmail.com>
2021-11-28 14:37:44 -0800

    forgot to save, but pages get own file

--- App.js
+++ App.js
@@ -92,19 +92,6 @@
     );
   }
 
-  // function HomeScreen({navigation}) {
-  //   return (
-  //     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-  //       <Text>Details Screen</Text>
-  //       <Button
-  //         title="Go to Details... again"
-  //         onPress={() => navigation.push('Temp')}
-  //       />
-  //       <Button title="Go to Auth" onPress={() => navigation.navigate('Auth')} />
-  //       <Button title="Go back" onPress={() => navigation.goBack()} />
-  //     </View>
-  //   );
-  // }
 
   function Scan({naviagation}) {
     return (
