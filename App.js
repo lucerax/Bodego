568e30565788a0e59726e9ce13a773a466cfcbb6
Shivang Singh <shivangsingh@gmail.com>
2021-11-28 01:29:40 -0800

    Gotta grind tmrw

--- App.js
+++ App.js
@@ -1,119 +1,162 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+  /**
+   * Sample React Native App
+   * https://github.com/facebook/react-native
+   *
+   * @format
+   * @flow strict-local
+   */
+  import React from 'react';
+  import LoginScreen from "react-native-login-screen";
+  import {
+    SafeAreaView,
+    ScrollView,
+    StatusBar,
+    StyleSheet,
+    Text,
+    useColorScheme,
+    View,
+    Button
+  } from 'react-native';
+  import { NavigationContainer } from '@react-navigation/native';
+  import {createNativeStackNavigator} from '@react-navigation/native-stack';
+  import {
+    Colors,
+    DebugInstructions,
+    Header,
+    LearnMoreLinks,
+    ReloadInstructions,
+  } from 'react-native/Libraries/NewAppScreen';
+  
+  import colors from './assets/colors/colors';
 
- import React from 'react';
+  const Stack = createNativeStackNavigator();
 
- import {
-   SafeAreaView,
-   ScrollView,
-   StatusBar,
-   StyleSheet,
-   Text,
-   useColorScheme,
-   View,
- } from 'react-native';
- 
- import {
-   Colors,
-   DebugInstructions,
-   Header,
-   LearnMoreLinks,
-   ReloadInstructions,
- } from 'react-native/Libraries/NewAppScreen';
- 
-//  import colors from './assets/colors/colors';
- 
- const Section = ({children, title}) => {
-   const isDarkMode = useColorScheme() === 'dark';
-   return (
-     <View style={styles.sectionContainer}>
-       <Text
-         style={[
-           styles.sectionTitle,
-           {
-             color: isDarkMode ? Colors.white : Colors.black,
-           },
-         ]}>
-         {title}
-       </Text>
-       <Text
-         style={[
-           styles.sectionDescription,
-           {
-             color: isDarkMode ? Colors.light : Colors.dark,
-           },
-         ]}>
-         {children}
-       </Text>
-     </View>
-   );
- };
- 
- const App = ()  => {
-   const isDarkMode = useColorScheme() === 'dark';
- 
-   const backgroundStyle = {
-     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-   };
-  //  const [email, setEmail] = useState('');
-  //  const [password, setPassword] = useState('');
- 
-   return (
-     <SafeAreaView style={backgroundStyle}>
-       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-       <ScrollView
-         contentInsetAdjustmentBehavior="automatic"
-         style={backgroundStyle}>
-         <Header />
-         <View
-           style={{
-             backgroundColor: isDarkMode ? Colors.black : Colors.white,
-           }}>
-           <Section title="Step One">
-             Edit <Text style={styles.highlight}>App.js</Text> to change this
-             screen and then come back to see your edits.
-           </Section>
-           <Section title="See Your Changes">
-             <ReloadInstructions />
-           </Section>
-           <Section title="Debug">
-             <DebugInstructions />
-           </Section>
-           <Section title="Learn More">
-             Read the docs to discover what to do next:
-           </Section>
-           <LearnMoreLinks />
-         </View>
-       </ScrollView>
-     </SafeAreaView>
-   );
- };
- 
- const styles = StyleSheet.create({
-   sectionContainer: {
-     marginTop: 32,
-     paddingHorizontal: 24,
-   },
-   sectionTitle: {
-     fontSize: 24,
-     fontWeight: '600',
-     fontFamily: 'Inter.Bold',
-     color: Colors.black,
-   },
-   sectionDescription: {
-     marginTop: 8,
-     fontSize: 18,
-     fontWeight: '400',
-   },
-   highlight: {
-     fontWeight: '700',
-   },
- });
- 
- export default App;
- 
+  const Section = ({children, title}) => {
+    const isDarkMode = useColorScheme() === 'dark';
+    return (
+      <View style={styles.sectionContainer}>
+        <Text
+          style={[
+            styles.sectionTitle,
+            {
+              color: isDarkMode ? Colors.white : Colors.black,
+            },
+          ]}>
+          {title}
+        </Text>
+        <Text
+          style={[
+            styles.sectionDescription,
+            {
+              color: isDarkMode ? Colors.light : Colors.dark,
+            },
+          ]}>
+          {children}
+        </Text>
+      </View>
+      // <LoginScreen />
+    );
+  };
+
+  const Auth = () => {
+    return (
+      <LoginScreen
+          labelTextStyle={{
+            color: "#adadad",
+            fontFamily: "Now-Bold",
+          }}
+          logoTextStyle={{
+            fontSize: 27,
+            color: "#fdfdfd",
+            fontFamily: "Now-Black",
+          }}
+          loginButtonTextStyle={{
+            color: "#fdfdfd",
+            // color: "#adadad",
+            fontFamily: "Now-Bold",
+          }}
+          textStyle={{
+            color: "#757575",
+            // color: "fdfdfdfd",
+            fontFamily: "Now-Regular",
+          }}
+          signupStyle={{
+            color: "#fdfdfd",
+            fontFamily: "Now-Bold",
+          }}
+          usernameOnChangeText={(username) => console.log("Username: ", username)}
+          onPressSettings={() => alert("Settings Button is pressed")}
+          passwordOnChangeText={(password) => console.log("Password: ", password)}
+          onPressLogin={() => {
+            console.log("onPressLogin is pressed");
+          }}
+          onPressSignup={() => {
+            console.log("onPressSignUp is pressed");
+          }}
+        >
+          <View
+            style={{
+              position: "relative",
+              alignSelf: "center",
+              marginTop: 64,
+            }}
+          >
+            <Text style={{ color: "white", fontSize: 30 }}>
+              Inside Login Screen Component
+            </Text>
+          </View>
+        </LoginScreen>
+    );
+  }
+
+  function Temp({navigation}) {
+    return (
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+        <Button
+          title="Go to Details... again"
+          onPress={() => navigation.navigate('Temp')}
+        />
+        <Button title="Go to Home" onPress={() => navigation.navigate('Home')} />
+        <Button title="Go back" onPress={() => navigation.goBack()} />
+      </View>
+    );
+  }
+  
+  const App = ()  => {
+  
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Temp" screenOptions={{headerShown: false}}>
+          <Stack.Screen name="Auth" component={Auth} />
+          {/* <Stack.Screen name = "Temp" component={Temp} /> */}
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+      
+  };
+  
+  const styles = StyleSheet.create({
+    sectionContainer: {
+      marginTop: 32,
+      paddingHorizontal: 24,
+    },
+    sectionTitle: {
+      fontSize: 24,
+      fontWeight: '600',
+      fontFamily: 'Inter-Bold',
+      color: '#5CDB95',
+    },
+    sectionDescription: {
+      marginTop: 8,
+      fontSize: 18,
+      fontWeight: '400',
+      color: '#5CDB95',
+    },
+    highlight: {
+      fontWeight: '700',
+    },
+  });
+  
+  export default App;
+  
--- assets/colors/colors.js
+++ assets/colors/colors.js
@@ -1,8 +1,8 @@
 const colors = {
-    main_color = "#5CDB95",
-    dark_accent = "#05386B",
-    background_light = "#F5F5F8",
-    white = "#FFFFFF",
+    mainGreen: "#5CDB95",
+    darkAccentBlue: "#05386B",
+    backgroundLight: "#F5F5F8",
+    white: "#FFFFFF",
 
 };
 
